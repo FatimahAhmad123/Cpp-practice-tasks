793a487fb2115cc6307d106b61afaeb51d1fdc5f
Fatimah <Fatimahse25@gmail.com>
2023-10-25 11:21:28 +0500

    Added Cmake Practice tasks

---
+++ Cmake-practice/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "calculator.h"
+
+int main()
+{
+	Calculator calc;
+	// int x;
+
+	int result_add = calc.add(5, 3);
+	int result_subtract = calc.subtract(5, 3);
+	int result_multiply = calc.multiply(5, 3);
+	double result_divide = calc.divide(5, 3);
+
+	std::cout << "Addition: " << result_add << std::endl;
+	std::cout << "Subtraction: " << result_subtract << std::endl;
+	std::cout << "Multiplication: " << result_multiply << std::endl;
+	std::cout << "Division: " << result_divide << std::endl;
+
+	return 0;
+}
